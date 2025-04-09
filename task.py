3b72d6f54482d12b3d3916ad7fd0eb74f91fb717
Aryan Shah <ashah148@asu.edu>
2025-04-08 22:14:12 -0700

    Add ability to view all tasks

--- task.py
+++ task.py
@@ -4,3 +4,8 @@ if sys.argv[1] == "add":
     with open("tasks.txt", "a") as f:
         f.write(" ".join(sys.argv[2:]) + "\n")
     print("Task added.")
+elif sys.argv[1] == "list":
+    with open("tasks.txt", "r") as f:
+        tasks = f.readlines()
+        for i, task in enumerate(tasks):
+            print(f"{i + 1}. {task.strip()}")
